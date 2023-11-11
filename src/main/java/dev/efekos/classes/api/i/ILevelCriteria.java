07ee04883fb83aed1cbec49b0c839ef526b93dee
efekos <hefeolgun10@gmail.com>
2023-11-11 17:43:57 +0300

    remove api (seperating to another package)

--- src/main/java/dev/efekos/classes/api/i/ILevelCriteria.java
+++
@@ -1,9 +0,0 @@
-package dev.efekos.classes.api.i;
-
-import org.bukkit.event.Event;
-import org.bukkit.event.player.PlayerEvent;
-
-public interface ILevelCriteria {
-     boolean shouldGiveExp(Event event);
-
-}
--- src/main/java/dev/efekos/classes/api/i/IPerk.java
+++
@@ -1,9 +0,0 @@
-package dev.efekos.classes.api.i;
-
-import org.bukkit.entity.Player;
-
-public interface IPerk {
-    void grant(Player player,int level);
-    void degrade(Player player);
-    String getDescription(int level);
-}
