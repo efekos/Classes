0351b9a1c2286a470888e1139013e86c56132757
efekos <56752400+efekos@users.noreply.github.com>
2024-08-30 21:06:53 +0300

    reduce lines in ClassesPerks

--- src/main/java/dev/efekos/classes/registry/ClassesPerks.java
+++ src/main/java/dev/efekos/classes/registry/ClassesPerks.java
@@ -4,9 +4,7 @@ import dev.efekos.classes.Main;
 import dev.efekos.classes.Utilities;
 import dev.efekos.classes.api.i.IPerk;
 import dev.efekos.classes.events.HandlingEvents;
-import dev.efekos.classes.registry.perk.AuraPerk;
-import dev.efekos.classes.registry.perk.LifeStealerPerk;
-import dev.efekos.classes.registry.perk.TimedEffectPerk;
+import dev.efekos.classes.registry.perk.*;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
@@ -20,237 +18,42 @@ public final class ClassesPerks {
         return Main.PERK_REGISTRY.register(new NamespacedKey(Main.getInstance(), name), modifier);
     }
 
-    public static final IPerk FAST_ARROW = register("fast_arrow", new IPerk() {
-        @Override
-        public void grant(Player player, int level) {
-            HandlingEvents.playersToBoostArrow.put(player, level);
-        }
-
-        @Override
-        public void degrade(Player player) {
-            HandlingEvents.playersToBoostArrow.remove(player);
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.fast_arrow", "Shoot arrows &a+%100&e faster");
-        }
-    });
-    public static final IPerk FAST_PEARL = register("fast_pearl", new IPerk() {
-        @Override
-        public void grant(Player player, int level) {
-            HandlingEvents.playersToBoostPearl.put(player, level);
-        }
-
-        @Override
-        public void degrade(Player player) {
-            HandlingEvents.playersToBoostPearl.remove(player);
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.fast_pearl", "Shoot ender pearls &a+%100&e faster");
-        }
-    });
-    public static final IPerk FAST_SNOWBALL = register("fast_snowball", new IPerk() {
-        @Override
-        public void grant(Player player, int level) {
-            HandlingEvents.playersToBoostSnowball.put(player, level);
-        }
-
-        @Override
-        public void degrade(Player player) {
-            HandlingEvents.playersToBoostSnowball.remove(player);
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.fast_snowball", "Shoot snowballs &a+%100&e faster");
-        }
-    });
+    public static final IPerk FAST_ARROW = register("fast_arrow", new MapManagedPerk(level -> Main.LANG.getString("perks.fast_arrow", "Shoot arrows &a+%100&e faster"),() -> HandlingEvents.playersToBoostArrow));
+    public static final IPerk FAST_PEARL = register("fast_pearl", new MapManagedPerk(level -> Main.LANG.getString("perks.fast_pearl", "Shoot ender pearls &a+%100&e faster"),() -> HandlingEvents.playersToBoostPearl));
+    public static final IPerk FAST_SNOWBALL = register("fast_snowball", new MapManagedPerk(level -> Main.LANG.getString("perks.fast_snowball", "Shoot snowballs &a+%100&e faster"),() -> HandlingEvents.playersToBoostSnowball));
     public static final LifeStealerPerk LIFE_STEALER = register("life_stealer", new LifeStealerPerk());
-    public static final TimedEffectPerk BURNER = register("burner", new TimedEffectPerk() {
-        @Override
-        public void grant(Player player, int level) {
+    public static final AffecterEffectPerk FROSTER = register("froster", new AffecterEffectPerk((entity, level) -> {
+        int bound = 5;
+        bound += (level / 5) * 3;
 
-        }
+        entity.setFreezeTicks(entity.getFreezeTicks() + bound);
+    },level -> Main.LANG.getString("perks.froster", "&eHitting an enemy freezes it.")));
+    public static final AffecterEffectPerk BURNER = register("burner", new AffecterEffectPerk((entity, level) -> {
+        int bound = 5;
+        bound += (level / 5) * 3;
 
-        @Override
-        public void degrade(Player player) {
+        entity.setFireTicks(entity.getFireTicks() + bound);
+    },level -> Main.LANG.getString("perks.burner", "&eHitting an enemy burns it.")));
+    public static final AffecterEffectPerk POISONER = register("poisoner", new AffecterEffectPerk((entity, level) -> {
+        if(!(entity instanceof LivingEntity e))return;
+        int bound = 5;
+        bound += (level / 5) * 3;
 
-        }
+        e.addPotionEffect(new PotionEffect(PotionEffectType.POISON,bound,Math.min(30,level)/3));
+    },level -> Main.LANG.getString("perks.poisoner", "&eHitting an enemy poisons it.")));
 
-        @Override
-        public void affectEntity(Entity entity, int level) {
-            int bound = 5;
-            bound += (level / 5) * 3;
+    public static final IPerk LAST_RUN = register("last_run", new HandledPerk(level -> Main.LANG.getString("perks.last_run", "Run faster when under &9%25&e health.")));
+    public static final IPerk LAST_ATTACK = register("last_attack", new HandledPerk(level -> Main.LANG.getString("perks.last_attack", "Deal more damage when under &9%25&e health.")));
+    public static final IPerk LAST_SHIELD = register("last_shield", new HandledPerk(level -> Main.LANG.getString("perks.last_shield", "Get Resistance effect when under &9%25&e health.")));
 
-            entity.setFireTicks(entity.getFireTicks() + bound);
-        }
+    public static final AffecterEffectPerk LOVE = register("love", new AffecterEffectPerk((entity, level) -> {
+        if (!(entity instanceof LivingEntity e)) return;
+        int a = 1+(level/10);
+        e.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, a * 100, a, false, false, false));
 
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.burner", "&eHitting an enemy burns it.");
-        }
-    });
-    public static final TimedEffectPerk FROSTER = register("froster", new TimedEffectPerk() {
-        @Override
-        public void grant(Player player, int level) {
+    },level -> Main.LANG.getString("perks.love", "Killing an entity gives Strength %a% effect. ")
+            .replace("%a%", Utilities.generateAmountText(1+(level/10), ChatColor.YELLOW + ""))));
 
-        }
-
-        @Override
-        public void degrade(Player player) {
-
-        }
-
-        @Override
-        public void affectEntity(Entity entity, int level) {
-            int bound = 5;
-            bound += (level / 5) * 3;
-
-            entity.setFireTicks(entity.getFireTicks() + bound);
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.froster", "&eHitting an enemy freezes it.");
-        }
-    });
-    public static final TimedEffectPerk POISONER = register("poisoner", new TimedEffectPerk() {
-        @Override
-        public void affectEntity(Entity entity, int level) {
-            if (!(entity instanceof LivingEntity e)) return;
-
-            int bound = 5;
-            bound += (level / 5) * 3;
-
-            int l = 0;
-            if (level >= 5) l++;
-            if (level >= 10) l++;
-            if (level >= 15) l++;
-            if (level >= 20) l++;
-            if (level >= 25) l++;
-            if (level >= 30) l++;
-            if (level >= 35) l++;
-            if (level >= 40) l++;
-            if (level >= 45) l++;
-            if (level >= 50) l++;
-            if (level >= 55) l++;
-            if (level >= 60) l++;
-            if (level >= 65) l++;
-            if (level >= 70) l++;
-            if (level >= 75) l++;
-            if (level >= 80) l++;
-            if (level >= 85) l++;
-            if (level >= 90) l++;
-            if (level >= 95) l++;
-            if (level >= 100) l++;
-
-            e.addPotionEffect(new PotionEffect(PotionEffectType.POISON, bound, l));
-        }
-
-        @Override
-        public void grant(Player player, int level) {
-
-        }
-
-        @Override
-        public void degrade(Player player) {
-
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.poisoner", "Hitting an entity poisons it.");
-        }
-    });
-    public static final IPerk LAST_RUN = register("last_run", new IPerk() {
-        @Override
-        public void grant(Player player, int level) {
-
-        }
-
-        @Override
-        public void degrade(Player player) {
-
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.last_run", "Run faster when under &9%25&e health.");
-        }
-    });
-    public static final IPerk LAST_ATTACK = register("last_attack", new IPerk() {
-        @Override
-        public void grant(Player player, int level) {
-
-        }
-
-        @Override
-        public void degrade(Player player) {
-
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.last_attack", "Deal more damage when under &9%25&e health.");
-        }
-    });
-    public static final IPerk LAST_SHIELD = register("last_shield", new IPerk() {
-        @Override
-        public void grant(Player player, int level) {
-
-        }
-
-        @Override
-        public void degrade(Player player) {
-
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.last_shield", "Get Resistance effect when under &9%25&e health.");
-        }
-    });
-    public static final TimedEffectPerk LOVE = register("love", new TimedEffectPerk() {
-        @Override
-        public void grant(Player player, int level) {
-
-        }
-
-        @Override
-        public void degrade(Player player) {
-
-        }
-
-        @Override
-        public void affectEntity(Entity entity, int level) {
-            if (!(entity instanceof LivingEntity e)) return;
-            e.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, getAmount(level) * 100, getAmount(level), false, false, false));
-        }
-
-        private int getAmount(int level) {
-            int el = 1;
-            if (level >= 10) el++;
-            if (level >= 20) el++;
-            if (level >= 30) el++;
-            if (level >= 40) el++;
-            if (level >= 50) el++;
-            if (level >= 60) el++;
-            if (level >= 70) el++;
-            if (level >= 80) el++;
-            if (level >= 90) el++;
-            if (level >= 100) el++;
-            return el;
-        }
-
-        @Override
-        public String getDescription(int level) {
-            return Main.LANG.getString("perks.love", "Killing an entity gives Strength %a% effect. ")
-                    .replace("%a%", Utilities.generateAmountText(getAmount(level), ChatColor.YELLOW + ""));
-        }
-    });
     public static final AuraPerk AURA = register("aura", new AuraPerk(Main.LANG.getString("perks.aura", "Blocks within a distance of %a% blocks get on fire out of nowhere")) {
         @Override
         public void affectBlock(World world, Block block) {
