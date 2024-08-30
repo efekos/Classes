6d000b24fd1342c1b280a73572cd9a4ca7d911aa
efekos <56752400+efekos@users.noreply.github.com>
2024-08-30 21:08:37 +0300

    remove TimedEffectPerk.java

--- src/main/java/dev/efekos/classes/registry/perk/TimedEffectPerk.java
+++
@@ -1,8 +0,0 @@
-package dev.efekos.classes.registry.perk;
-
-import dev.efekos.classes.api.i.IPerk;
-import org.bukkit.entity.Entity;
-
-public interface TimedEffectPerk extends IPerk {
-    void affectEntity(Entity entity, int level);
-}
